be616138f19c60dd66dda6c4f455edcc155da66c
Jukka Villstedt <jukka.villstedt@iki.fi>
2025-01-21 20:57:31 +0200

    times table animations

--- src/hitype/times_table.clj
+++ src/hitype/times_table.clj
@@ -23,22 +23,23 @@
                  :button-color [0 0 0 255]
                  :event-description-color [70 50 30]})
 
-(def theme {:text-color [0 0 0 255]
-            :background-color [255 255 255 255]
-            :button-color [200 200 255 255]
-            :event-description-color [10 60 10]})
+(def light-theme {:text-color [0 0 0 255]
+                  :background-color [255 255 255 255]
+                  :button-color [200 200 255 255]
+                  :event-description-color [10 60 10]})
+(def theme dark-theme)
 
 (defn teksti [teksti & [koko väri]]
   (visuals/text-area (str teksti)
                      (if väri
-                       (three-number-color väri)
+                       väri
                        (:text-color theme))
                      (font/create-by-name "Serif" (or koko tekstin-koko))
                      #_(visuals/liberation-sans-regular (or koko tekstin-koko))))
 
 
 
-(def exercises (filter #(and (some  #{7} [(:x %) (:y %)])
+(def exercises (filter #(and (some  #{4} [(:x %) (:y %)])
                              (<= (:x %)
                                  (:y %)))
                        (for [x (range 2 10)
@@ -60,20 +61,27 @@
                              (concat [right-answer])
                              (shuffle))))))
 
+(def maximum-exercise-points 3)
+
 (defn next-exercise [state]
-  (let [candidates (->> exercises
-                        (remove (fn [exercise]
-                                  (or (= exercise (:exercise state))
-                                      (< 4 (or (get-in state [:points exercise])
-                                               0)))))
-                        (take 3))]
+  (let [candidates (let [unfinished-exercises (remove (fn [exercise]
+                                                        (<= maximum-exercise-points
+                                                            (or (get-in state [:points exercise])
+                                                                0)))
+                                                      exercises)]
+                     (if (= unfinished-exercises
+                            [(:exercise state)])
+                       [(:exercise state)]
+                       (->> unfinished-exercises
+                            (remove #{(:exercise state)})
+                            (take 3))))]
     (when (not (empty? candidates))
       (rand-nth candidates))))
 
 (def answer-keys [:a :s :d :f])
 
 (def anwser-key-to-option-index (into {} (map vector answer-keys
-                                     (range 4))))
+                                              (range 4))))
 
 (defonce points-atom (atom nil))
 
@@ -82,74 +90,78 @@
   )
 
 (defn- game-view  [state]
-  (prn '(animation/running? @animation/state-atom  :finish) (animation/running? @animation/state-atom  :finish)) ;; TODO: remove me
+  (let [finish-phase (or (animation/repeating-phase! :finish 2000)
+                         0)]
+    (layouts/superimpose (visuals/rectangle-2 :fill-color
+                                              (:background-color theme))
+                         (layouts/center-horizontally
 
-  (layouts/superimpose (visuals/rectangle-2 :fill-color
-                                            (:background-color theme))
-                       (layouts/center-horizontally
-                        (layouts/with-margins (animation/linear-mapping (animation/ease-in-cubic (or (animation/phase! :finish 1000)
-                                                                                                     0))
-                                                                        0
-                                                                        2000)
-                          0 0 0
-                          (layouts/with-margin 50
-                            (let [{:keys [x y]} (:exercise state)]
-                              (layouts/vertically-2 {:margin 20 :centered? true}
-                                                    (teksti (str x " * " y))
-                                                    (layouts/grid [(map (fn [value]
-                                                                          (layouts/with-margin 10 (teksti value))) (:options state))
-                                                                   (map (fn [anser-key]
-                                                                          (layouts/with-margin 10 (teksti (name anser-key))))
-                                                                        answer-keys)])
-                                                    ;; (layouts/horizontally-2 {:margin 10}
-                                                    ;;                         (for [[key option] (map vector
-                                                    ;;                                                 answer-keys
-                                                    ;;                                                 (:options state))]
-                                                    ;;                           (teksti (str (name key) " = " option))))
+                          (when (> 1 finish-phase)
+                            (layouts/with-margins (-> finish-phase
+                                                      (animation/exponential-ease-in 8)
+                                                      (animation/linear-mapping 0 2000))
+                              0 0 0
+                              (layouts/with-margin 50
+                                (let [{:keys [x y]} (:exercise state)]
+                                  (layouts/vertically-2 {:margin 20 :centered? true}
+                                                        (teksti (str x " * " y))
+                                                        (layouts/grid [(map (fn [value]
+                                                                              (layouts/with-margin 10 (teksti value))) (:options state))
+                                                                       (map (fn [anser-key]
+                                                                              (layouts/with-margin 10 (teksti (name anser-key))))
+                                                                            answer-keys)])
+                                                        (layouts/horizontally-2 {:margin 50}
+                                                                                (layouts/vertically-2 {:margin 5 :centered? true}
+                                                                                                      (for [exercise (take (/ (count exercises)
+                                                                                                                              2)
+                                                                                                                           exercises)]
+                                                                                                        (teksti (str (:x exercise) " * " (:y exercise)
+                                                                                                                     " : "
+                                                                                                                     (or (get (:points state)
+                                                                                                                              exercise)
+                                                                                                                         0)
+                                                                                                                     #_" pistettä." ))))
+                                                                                (layouts/vertically-2 {:margin 5 :centered? true}
+                                                                                                      (for [exercise (drop (/ (count exercises)
+                                                                                                                              2)
+                                                                                                                           exercises)]
+                                                                                                        (teksti (str (:x exercise) " * " (:y exercise)
+                                                                                                                     " : "
+                                                                                                                     (or (get (:points state)
+                                                                                                                              exercise)
+                                                                                                                         0)
+                                                                                                                     #_" pistettä." )))))
+                                                        (teksti (str "Yhteensä: " (apply + (vals (:points state)))))))))))
 
-                                                    ;; (for [[exercise points] (:points state)]
-                                                    ;;   (teksti (str (:x exercise) " * " (:y exercise) " : " points " pistettä." )))
-
-                                                    (layouts/horizontally-2 {:margin 50}
-                                                                            (layouts/vertically-2 {:margin 5 :centered? true}
-                                                                                                  (for [exercise (take (/ (count exercises)
-                                                                                                                          2)
-                                                                                                                       exercises)]
-                                                                                                    (teksti (str (:x exercise) " * " (:y exercise)
-                                                                                                                 " : "
-                                                                                                                 (or (get (:points state)
-                                                                                                                          exercise)
-                                                                                                                     0)
-                                                                                                                 #_" pistettä." ))))
-                                                                            (layouts/vertically-2 {:margin 5 :centered? true}
-                                                                                                  (for [exercise (drop (/ (count exercises)
-                                                                                                                          2)
-                                                                                                                       exercises)]
-                                                                                                    (teksti (str (:x exercise) " * " (:y exercise)
-                                                                                                                 " : "
-                                                                                                                 (or (get (:points state)
-                                                                                                                          exercise)
-                                                                                                                     0)
-                                                                                                                 #_" pistettä." )))))
-                                                    (teksti (str "Yhteensä: " (apply + (vals (:points state))))))))))
-
-                       (when (animation/running? @animation/state-atom :finish)
-                         (layouts/center (assoc (teksti "Valmis!")
-                                                :x 500
-                                                :y (animation/linear-mapping (animation/ease-out-cubic (mod (animation/phase! :finish 1000)
-                                                                                                            1))
-                                                                             -50
-                                                                             500))))
+                         (when (animation/running? @animation/state-atom :finish)
+                           (layouts/center-horizontally
+                            (assoc (teksti "Valmis!")
+                                   :y (animation/linear-mapping (animation/exponential-ease-out finish-phase
+                                                                                                3)
+                                                                -1500
+                                                                300))))
 
 
-                       (when (and (not (:finished? state))
-                                  (animation/running? @animation/state-atom  :right-answer)
-                                  (> 1 (animation/phase! :right-answer 1000)))
-                         (assoc (teksti "Jee")
-                                :x 200
-                                :y (- 300 (animation/linear-mapping (animation/ease-out-cubic (animation/phase! :right-answer 1000))
-                                                                    0
-                                                                    200))))))
+                         (when (and (not (:finished? state))
+                                    (animation/running? @animation/state-atom  :right-answer))
+                           (layouts/center-horizontally
+                            (let [phase (animation/phase! :right-answer 2000)
+                                  color [150 150 150 (- 255
+                                                        (-> (animation/exponential-ease-in phase 2)
+                                                            (animation/linear-mapping 0 155)))]
+
+                                  y (- 400 (-> (animation/exponential-ease-out phase 3)
+                                               (animation/linear-mapping 0 300)))]
+                              (when (> 1 phase)
+                                (layouts/superimpose (assoc (teksti "Jee" 50 color)
+                                                            :x 0
+                                                            :y y)
+                                                     (assoc (teksti "Jee" 50 color)
+                                                            :x 500
+                                                            :y y)))))))))
+
+
+(def initial-state {:points (read-string (slurp "time-table-points.edn"))})
 
 (defn event-handler [state-atom _node event]
   (let [state @state-atom]
@@ -173,17 +185,24 @@
                               (if-some [exercise (next-exercise state)]
                                 (initialize-exercise state
                                                      exercise)
-                                (assoc state :finished? true)))))
-        (when (:finished? @state-atom)
-          (animation/swap-state! animation/start :finish))
+                                (do (animation/swap-state! animation/start :finish)
+;;                                     (.start (Thread. (fn []
+;;                                                        (prn "sleeping")
+
+;;                                                        (Thread/sleep 5000)
+;;                                                        (prn "resetting")
+;;                                                        (try
+;;                                                          (animation/swap-state! animation/delete :finish)
+;;                                                          (catch Exception e
+;;                                                              (prn e) ;; TODO: remove me
+;; ))                                                       (prn "phase " (animation/phase! :finish 1000))
+;;                                                        (reset! state-atom initial-state))))
+                                    (assoc state :finished? true))))))
+
         (reset! points-atom (:points @state-atom))))))
 
 (defn root-view []
-  (let [state-atom (atom (let [state #_{:points {} #_(read-string (slurp "time-table-points.edn"))}
-                               (do (animation/swap-state! animation/start :finish)
-                                   (println "started finish" (:time @animation/state-atom))
-                                   (prn '(animation/running? @animation/state-atom :finish) (animation/running? @animation/state-atom :finish)) ;; TODO: remove me
-                                   {:finished? true})]
+  (let [state-atom (atom (let [state initial-state]
                            (initialize-exercise state
                                                 (next-exercise state))))]
     (fn []
